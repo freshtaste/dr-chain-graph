b3ce068a1bb932976f02aba90cb9fc5a53e3cbb6
freshtaste <liujizhou123@hotmail.com>
2025-05-15 18:43:27 -0700

    get ready for cluster

--- utils.py
+++ utils.py
@@ -143,3 +143,15 @@ def compute_stats_ag(results1, results2, ground_truth1,):
                                 ground_truth1['indirect'], ground_truth1['spillover_effect']])
     }
     return ret_
+
+
+import os
+
+def delete_all_files_in_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}: {e}")
